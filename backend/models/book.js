d7725c274a66ad8fcbfa2656cf3bb550622ee8a7
JesusFern <jfr16sevilla@gmail.com>
2025-04-18 21:24:33 +0200

    fix: cambios en modelo de book

--- backend/models/book.js
+++ backend/models/book.js
@@ -5,6 +5,15 @@ const bookSchema = new mongoose.Schema({
   author: { type: String, required: true },
   genre: { type: String, required: true },
   summary: { type: String },
+  language: { type: String, default: 'Español' },
+  price: { type: Number, required: true },
+  format: [{ type: String, enum: ['PDF', 'EPUB', 'MOBI'] }], // formatos disponibles
+  coverImageUrl: { type: String }, // imagen de portada (url)
+  downloadFileUrls: {
+    PDF: { type: String },
+    EPUB: { type: String },
+    MOBI: { type: String }
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
