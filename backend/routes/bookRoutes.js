cbef473859a2b89ec6c5b138b674d4ef3f863bff
JesusFern <jfr16sevilla@gmail.com>
2025-04-20 16:46:10 +0200

    feat: creados gets por varios parametros

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -3,8 +3,13 @@ const {
     createBook, 
     importBooksController, 
     searchByTitle, 
-    multiMatchSearchWithPagination 
+    multiMatchSearchWithPagination,
+    searchByAuthor,
+    searchByGenre,
+    searchByPriceRange,
+    fuzzySearchByTitle
 } = require('../controllers/bookController');
+
 const router = express.Router();
 
 // Ruta para crear un libro
@@ -15,6 +20,10 @@ router.post('/import-books', importBooksController);
 
 // Rutas para buscar libros
 router.get('/search/title', searchByTitle);
+router.get('/search/author', searchByAuthor);
+router.get('/search/genre', searchByGenre);
+router.get('/search/price-range', searchByPriceRange);
 router.get('/search/pagination', multiMatchSearchWithPagination);
+router.get('/search/fuzzy-title', fuzzySearchByTitle);
 
 module.exports = router;
