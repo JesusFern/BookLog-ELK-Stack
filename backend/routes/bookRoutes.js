b0938f21d5e46ae9cbe01573d9a4fdfb0ef92d3e
JesusFern <jfr16sevilla@gmail.com>
2025-04-24 19:22:38 +0200

    fix: ahora multimatch tiene fuzziness

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -3,14 +3,15 @@ const {
     createBook, 
     importBooksController, 
     searchByTitle, 
-    multiMatchSearchWithPagination,
+    multiMatchFuzzySearch,
     searchByAuthor,
     searchByGenre,
     searchByPriceRange,
     fuzzySearchByTitle,
     getBooks,
     getBookById,
-    getTopBooks
+    getTopBooks,
+    getRelatedBooks
 } = require('../controllers/bookController');
 
 const router = express.Router();
@@ -26,10 +27,12 @@ router.get('/search/title', searchByTitle);
 router.get('/search/author', searchByAuthor);
 router.get('/search/genre', searchByGenre);
 router.get('/search/price-range', searchByPriceRange);
-router.get('/search/pagination', multiMatchSearchWithPagination);
+router.get('/search/pagination', multiMatchFuzzySearch);
 router.get('/search/fuzzy-title', fuzzySearchByTitle);
 router.get('/books', getBooks);
 router.get('/top-books', getTopBooks);
+
+router.get('/related/:bookId', getRelatedBooks);
 router.get('/:id', getBookById);
 
 
